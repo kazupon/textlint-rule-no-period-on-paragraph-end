829a865ab27d7e6d0784b584d201ad22bdd3f3c5
kazuya kawaguchi <kawakazu80@gmail.com>
2024-05-22 19:48:14 +0900

    feat: no perion on paragraph end (#4)
    * feat: no perion on paragraph end
    
    * fix: implementation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,6 +21,20 @@ export default [
         project: true,
         tsconfigRootDir: import.meta.dirname
       }
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ]
     }
   }
 ]
